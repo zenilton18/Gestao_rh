8de623ae8ef85dacaa28cd97629e5b738efbebd3
zenilton <ze18.fv@gmail.com>
2025-07-09 16:12:06 -0300

    conexão db e migrations

---
+++ database/migrations/2025_07_09_190009_create_user_details_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('user_details', function (Blueprint $table) {
+            $table->id();
+            $table->bigInteger('user_id');
+            $table->string('address', 100);
+            $table->string('zip_code', 10);
+            $table->string('city', 50);
+            $table->string('phone', 20);
+            $table->decimal('salary', 10, 2);
+            $table->date('admission_date');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('user_details');
+    }
+};
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,19 @@
 <?php
 
+
+use Illuminate\Mail\Message;
+use Illuminate\Support\Facades\Mail;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
+
+    
+    Mail::raw('teste gmaail', function(Message $message){
+        $message->to('teste@gmail.com')
+        ->subject('texto da mensagem')
+        ->from('rr@gmail.com');
+
+
+    });
     echo 'aqui';
 });
